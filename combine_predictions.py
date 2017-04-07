7044e2b4f9a0ce36950b6d384b22e25f40d11dc9
Sahil Gupta <sjgupta2@illinois.edu>
2017-04-06 19:12:14 -0500

    combine file changed

--- combine_predictions.py
+++ combine_predictions.py
@@ -1,5 +1,5 @@
 #combine predictions
-file_name = 'predicted_ratings_'
+file_name = './Results/predicted_ratings_'
 filenames = [file_name + str(i) + '.txt' for i in range(33)]
 #filenames = ['predicted_ratings_0.txt', 'predicted_ratings_1.txt']
 with open('predicted_ratings_combined.txt', 'w') as outfile:
